3324639f9a3c2a8c33619edd08897dccebf064af
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2023-08-21 15:59:06 +0530

    removed unwantedCode

--- src/Follower.js
+++ src/Follower.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
 const Follower = ({ avatar_url, html_url, login }) => {
-  // console.log('avatar_url',avatar_url);
-  // console.log('html_url',html_url);
-  // console.log('login',login);
   return (
     <article className='card'>
       <img src={avatar_url} alt={login} />
